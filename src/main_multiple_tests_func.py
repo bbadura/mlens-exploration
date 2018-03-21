8bfa2e5b9932dea776068c2e113d89f6f857b38e
Bryce Badura <brycebadura@Bryces-MacBook-Pro.local>
2018-03-21 13:08:45 -0700

    ensemble_collection complete (running on local)

--- src/main_multiple_tests_func.py
+++ src/main_multiple_tests_func.py
@@ -181,8 +181,73 @@ def add_ensemble_random(name, meta_name, meta, cross_val):
 		return {"Ensemble_Elements": name, "Meta_Classifier": meta_name, "Accuracy_Score_CV": "N/A", "Accuracy_Score": acc_score, "Runtime": time_}
 
 # Method to run a collection of models of different types
-#def ensemble_collection():
+def ensemble_collection(elements, meta_name, meta, cross_val):
+	# Establish and reset variables
+	acc_score_cv = None
+	acc_score = None
+	time_ = None
+	ensemble_cv = SuperLearner(scorer=accuracy_score, random_state=seed)
+	ensemble = SuperLearner(scorer=accuracy_score, random_state=seed)
+	models = []
 
+	for element in elements:
+		#try out a new classifier
+		pipeline1 = Pipeline([
+			(element[0], element[1])
+		])
+		models.append(pipeline1)
+
+	if (cross_val == True):
+		ensemble_cv.add(models)
+		ensemble.add(models)
+
+		# Attach the final meta estimator
+		ensemble_cv.add_meta(meta)
+		ensemble.add_meta(meta)
+
+		ensemble_cv.fit(X_train[i][:1600], Y_train[i][:1600])
+		preds_cv = ensemble_cv.predict(X_test[i][:1600])
+		acc_score_cv = accuracy_score(preds_cv, Y_test[i][:1600])
+
+		start = time.time()
+		ensemble.fit(X_train[i], Y_train[i])
+		preds = ensemble.predict(X_test[i])
+		acc_score = accuracy_score(preds, Y_test[i])
+		end = time.time()
+		time_ = end - start
+		if(acc_score > 0.83):
+			if (len(listToSave) == 5):
+				for x in listToSave:
+					if x[1] < acc_score:
+						x = [ensemble, acc_score]
+						break;
+			else:
+				listToSave.append([ensemble, acc_score])
+		listToSave.sort(key=itemgetter(1))
+
+		return {"Ensemble_Elements": elements, "Meta_Classifier": meta_name, "Accuracy_Score_CV": acc_score_cv, "Accuracy_Score": acc_score, "Runtime": time_}
+	else:
+		ensemble.add(models)
+		# Attach the final meta estimator
+		ensemble.add_meta(meta)
+
+		start = time.time()
+		ensemble.fit(X_train[i], Y_train[i])
+		preds = ensemble.predict(X_test[i])
+		acc_score = accuracy_score(preds, Y_test[i])
+		end = time.time()
+		time_ = end - start
+		if(acc_score > 0.83):
+			if (len(listToSave) == 5):
+				for x in listToSave:
+					if x[1] < acc_score:
+						x = [ensemble, acc_score]
+						break;
+			else:
+				listToSave.append([ensemble, acc_score])
+		listToSave.sort(key=itemgetter(1))
+
+		return {"Ensemble_Elements": elements, "Meta_Classifier": meta_name, "Accuracy_Score_CV": "N/A", "Accuracy_Score": acc_score, "Runtime": time_}
 
 
 # Method to run old ensembles that performed well
@@ -259,13 +324,18 @@ for i in range(len(combine)):
 
 output = {} 
 
-output['rfc'] = add_ensemble_same('rfc', RandomForestClassifier(n_estimators=random.randint(50,150),max_features=random.randint(1,20),max_depth=random.randint(1,200),random_state=random.randint(1,5000)), 'svc', SVC(), False)
-output['lr'] = add_ensemble_same('lr', LogisticRegression(random_state=random.randint(1,5000)), 'svc', SVC(), False)
-output['etc'] = add_ensemble_same('etc', ExtraTreeClassifier(max_features=random.randint(1,20),max_depth=random.randint(1,200),random_state=random.randint(1,5000)), 'svc', SVC(), False)
-output['svc'] = add_ensemble_same('svc', SVC(random_state=random.randint(1,5000),degree=random.randint(1,5000)), 'svc', SVC(), False)
-output['knc'] = add_ensemble_same('knc', KNeighborsClassifier(n_neighbors=random.randint(1,20),leaf_size=random.randint(10,100)), 'svc', SVC(), False)
-output['dtc'] = add_ensemble_same('dtc', DecisionTreeClassifier(max_depth=random.randint(1,200),max_features=random.randint(1,20),random_state=random.randint(1,5000)), 'svc', SVC(), False)
+#output['rfc'] = add_ensemble_same('rfc', RandomForestClassifier(n_estimators=random.randint(50,150),max_features=random.randint(1,20),max_depth=random.randint(1,200),random_state=random.randint(1,5000)), 'svc', SVC(), False)
+#output['lr'] = add_ensemble_same('lr', LogisticRegression(random_state=random.randint(1,5000)), 'svc', SVC(), False)
+#output['etc'] = add_ensemble_same('etc', ExtraTreeClassifier(max_features=random.randint(1,20),max_depth=random.randint(1,200),random_state=random.randint(1,5000)), 'svc', SVC(), False)
+#output['svc'] = add_ensemble_same('svc', SVC(random_state=random.randint(1,5000),degree=random.randint(1,5000)), 'svc', SVC(), False)
+#output['knc'] = add_ensemble_same('knc', KNeighborsClassifier(n_neighbors=random.randint(1,20),leaf_size=random.randint(10,100)), 'svc', SVC(), False)
+#output['dtc'] = add_ensemble_same('dtc', DecisionTreeClassifier(max_depth=random.randint(1,200),max_features=random.randint(1,20),random_state=random.randint(1,5000)), 'svc', SVC(), False)
 #output['rp1'] = add_ensemble_random('rp1', 'svc', SVC(), False)
+output['combo'] = ensemble_collection([['rfc', RandomForestClassifier(n_estimators=random.randint(50,150),max_features=random.randint(1,20),max_depth=random.randint(1,200),random_state=random.randint(1,5000))],
+		['lr', LogisticRegression(random_state=random.randint(1,5000))],
+		['knc', KNeighborsClassifier(n_neighbors=random.randint(1,20),leaf_size=random.randint(10,100))],
+		['dtc', DecisionTreeClassifier(max_depth=random.randint(1,200),max_features=random.randint(1,20),random_state=random.randint(1,5000))]],
+		'svc', SVC(), False)
 
 t = Texttable()
 t.add_row(['Dataset', 'Ensemble Components', 'Meta Classifier', 'Accuracy Score', 'Accuracy Score (cross-val)', 'Runtime'])
